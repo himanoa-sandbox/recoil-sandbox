f146351355b82267c212f2cdf6271083b9c42322
himanoa <matsunoappy@gmail.com>
2020-05-17 02:58:17 +0900

    owaowari

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from "recoil"
+
+
+const sleep = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve()
+    }, 3000)
+  })
+}
+const textState = atom({
+  key: "textState",
+  default: ""
+})
+const asyncSelector = selector({
+  key: "charCountState",
+  get: async ({get}: any) => {
+    await sleep()
+    return 12
+  }
+})
+
+const throwExceptionSelector = selector({
+  key: "throwExceptionSelector",
+  get: async ({get}: any) => {
+    throw Error("error")
+  }
+})
+
+function CharacterCount() {
+  const count  = useRecoilValue(throwExceptionSelector)
+  return <>Count: {count}</>
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState)
+  const onChange = (e: any) => {
+    setText(e.target.value)
+  }
+  return <input onChange={onChange} value={text}></input>
+}
+
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <React.Suspense fallback={<div>Loading</div>}>
+        <CharacterCount />
+      </React.Suspense>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        <CharacterCounter />   
+      </div>
+    </RecoilRoot>
   );
 }
 
